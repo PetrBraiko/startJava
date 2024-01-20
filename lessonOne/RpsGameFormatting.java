9afda2c6b36e911a66c34a48ceb2da652e3bc103
PetrBraiko <brajkopetr@gmail.com>
2024-01-20 13:11:19 +0300

    fix

--- lessonOne/RpsGameFormatting.java
+++ lessonOne/RpsGameFormatting.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+
 public class RpsGameFormatting {
     public static void main(String[] args) throws InterruptedException {
         String rock = "\u270A";
@@ -7,49 +8,39 @@ public class RpsGameFormatting {
 
         // Ход первого игрока
         String name1 = "HEL";
-        System.out.println("Ход " + name1 + ": ");
         Random r = new Random();
         int position1 = r.nextInt(100);
-        String sign1;
+        String sign1 = rock;
+
         if (position1 > 66) {
             sign1 = paper;
         } else if (position1 > 33) {
             sign1 = scissors;
-        } else {
-            sign1 = rock;
         }
 
         // Визуализация выбора первого игрока
+        System.out.println("Ход " + name1 + ": ");
         for (int i = 0; i < 5; i++) {
-            System.out.print(rock + "\r");
-            Thread.sleep(100);
-            System.out.print(scissors + "\r");
-            Thread.sleep(100);
-            System.out.print(paper + "\r");
+            System.out.print(sign1 + "\r");
             Thread.sleep(100);
         }
         System.out.println(sign1);
 
         // Ход второго игрока
         String name2 = "WALLE";
-        System.out.println("Ход " + name2 + ": ");
         int position2 = r.nextInt(100);
-        String sign2;
+        String sign2 = rock;
+
         if (position2 > 66) {
             sign2 = paper;
         } else if (position2 > 33) {
             sign2 = scissors;
-        } else {
-            sign2 = rock;
         }
 
         // Визуализация выбора второго игрока
+        System.out.println("Ход " + name2 + ": ");
         for (int i = 0; i < 5; i++) {
-            System.out.print(rock + "\r");
-            Thread.sleep(100);
-            System.out.print(scissors + "\r");
-            Thread.sleep(100);
-            System.out.print(paper + "\r");
+            System.out.print(sign2 + "\r");
             Thread.sleep(100);
         }
         System.out.println(sign2);
@@ -58,9 +49,11 @@ public class RpsGameFormatting {
         if (sign1.equals(sign2)) {
             System.out.println("Победила дружба!");
         } else {
-            if ((sign1.equals(rock) && sign2.equals(scissors)) ||
+            boolean isName1Equal = (sign1.equals(rock) && sign2.equals(scissors)) ||
                     (sign1.equals(scissors) && sign2.equals(paper)) ||
-                    (sign1.equals(paper) && sign2.equals(rock))) {
+                    (sign1.equals(paper) && sign2.equals(rock));
+
+            if (isName1Equal) {
                 System.out.println("\nПобедил - " + name1);
             } else {
                 System.out.println("\nПобедил - " + name2);
