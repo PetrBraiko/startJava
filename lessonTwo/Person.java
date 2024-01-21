437bc5e2ca37e0b49464cb3e408e39644e49c636
PetrBraiko <brajkopetr@gmail.com>
2024-01-21 12:01:33 +0300

    fix

--- lessonTwo/Person.java
+++ lessonTwo/Person.java
@@ -14,19 +14,19 @@ public class Person {
         this.age = 32;
     }
         //методы описывающие поведение
-        public void walk() {
-            System.out.println(name + " идет.");
-        }
-        public void sit() {
-            System.out.println(name + " сидит.");
-        }
-        public void run() {
-            System.out.println(name + " бежит.");
-        }
-        public void speak() {
-            System.out.println(name + " говорит.");
-        }
-        public void learnJava() {
-            System.out.println(name + " изучает Java.");
-        }
+    public void walk() {
+        System.out.println(name + " идет.");
+    }
+    public void sit() {
+        System.out.println(name + " сидит.");
+    }
+    public void run() {
+        System.out.println(name + " бежит.");
+    }
+    public void speak() {
+        System.out.println(name + " говорит.");
+    }
+    public void learnJava() {
+        System.out.println(name + " изучает Java.");
+    }
 }
